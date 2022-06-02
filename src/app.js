7dab78704739e47a04b7d2a800b28b2f4ad77d18
c-chimie <cchimie303@gmail.com>
2022-06-02 00:12:32 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainName = [];
+  let pronoun = ["the", "our"];
+  let adj = ["fast", "hungry"];
+  let noun = ["computer", "ostrich"];
+  let domain = [".com", ".net"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          domainName.push(" " + pronoun[i] + adj[j] + noun[k] + domain[l]);
+        }
+      }
+    }
+  }
+  console.log(domainName);
+  document.getElementById("domain").innerHTML = domainName;
+  // document.write(domainName.join(""));
 };
